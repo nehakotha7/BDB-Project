f89ef70e64a45e315b0b5de0cdc0c8f18edaab8b
Neha Kotha <nehakotha7@gmail.com>
2024-11-19 23:22:31 -0500

    More visuals: EPA/play on avg amount of pass rushers

--- Kotha/BDB_Project.R
+++ Kotha/BDB_Project.R
@@ -6,11 +6,30 @@ library(tidyverse)
 
 library(help = "nflreadr")
 
-load_
-
 # nflreadr::load_data()
 
-pbp <- load_participation(include_pbp = T)
+pbp <- load_participation(season = 2023, include_pbp = TRUE)
+View(pbp)
+
+pass_rush_epa <- pbp |>
+  filter (pass == 1) |> 
+  group_by(defteam) |> 
+  summarise(plays=n(),
+            avg_pass_rushers = round(mean(number_of_pass_rushers,na.rm=T),3),
+            epa = mean (epa, na.rm=T))
+
+ggplot2::ggplot(pass_rush_epa,aes(x=avg_pass_rushers, y=epa)) +
+  geom_mean_lines(aes(x0=avg_pass_rushers, y0= epa)) +
+  geom_nfl_logos(aes(team_abbr = defteam),width=0.055) + 
+  theme_classic()+
+  ggtitle("EPA/play on Average Amount of Pass Rushers",
+          "2023 Season Through Week 12") +
+  xlab('Average Amount of Pass Rushers') +
+  ylab('EPA/play') +
+  labs(caption = 'Data: nflreadr')
+
+participation <- load_participation(seasons = 2023)
+View(participation)
 
 rosters_2023 <- load_rosters(seasons = 2023)
 View(rosters_2023)
